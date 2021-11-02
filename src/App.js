f99527d583208c71ff40e87841faa19343f38675
Yonatan Haddish <yonihailshn@gmail.com>
2021-11-02 12:00:04 -0400

    component trial working well

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
    <div>
-     <h1>Hello World!</h1>
+     <Navbar />
    </div>
   );
 }
