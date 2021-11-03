4e3ee7d3461897bcdd1a97ae5569c31d6d9dc98f
Yonatan Haddish <yonihailshn@gmail.com>
2021-11-03 14:28:29 -0400

    side bar styleing

--- src/components/Navbar/Sidebar/SidebarElements.js
+++ src/components/Navbar/Sidebar/SidebarElements.js
@@ -11,7 +11,8 @@ export const SidebarContainer = styled.aside `
     align-items: center;
     left: 0;
     transition: 0.3s ease-in-out;
-    top: 0;
+    opacity: ${({isOpen}) => (isOpen ? '100%' : '0')};
+    top: ${({isOpen}) => (isOpen ? '0' : '-100%')};
 `
 
 export const CloseIcon = styled(FaTimes) `
