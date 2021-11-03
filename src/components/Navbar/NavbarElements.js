f4df27b533b0a4db121a18a11fe1292a0aef6d42
Yonatan Haddish <yonihailshn@gmail.com>
2021-11-03 12:01:14 -0400

    nav bar creating

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { Link as LinkR } from 'react-router-dom';
+import { Link as LinkS } from "react-scroll";
 
 export const Nav = styled.nav `
     background-color: #000;
@@ -37,4 +38,49 @@ export const NavLogo = styled(LinkR)`
     marigin-left: 24px;
     font-weight: bold;
     text-decoration: none;
+`
+
+export const MobileIcon =  styled.div `
+    display: none;
+
+    @media screen and (max-width: 768px) {
+        display: block;
+        position: absolute;
+        top: 0;
+        right: 0;
+        trasnform: translate(-100%, 60%);
+        font-size: 1.8rem;
+        cursor: pointer;
+        color: #fff;
+    }
+`
+
+export const NavMenu = styled.ul `
+    display: flex;
+    align-items: center;
+    list-style: none;
+    text-align: center;
+    margin-right: -22px;
+
+    @media screen and (max-width: 768px) {
+        display: none;
+    }
+`
+
+export const NavItem = styled.li `
+    height: 80px;
+`
+
+export const NavLinks = styled(LinkS) `
+    color: #fff;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    padding: 0 1rem;
+    height: 100%;
+    cursor: pointer;
+
+    &.active {
+        border-buttom: 3px solid #01bf71;
+    }
 `
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,16 +1,42 @@
-import React from 'react'
-import { Nav, NavbarContainer, NavLogo } from './NavbarElements';
+import React from "react";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+} from "./NavbarElements";
+import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
-    return (
-        <>
-            <Nav>
-                <NavbarContainer>
-                    <NavLogo to="/">yh</NavLogo>
-                </NavbarContainer>
-            </Nav>
-        </>
-    );
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">yh</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="discover">Discover</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="services">Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="signup">Sign up</NavLinks>
+            </NavItem>
+          </NavMenu>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
 };
 
 export default Navbar;
