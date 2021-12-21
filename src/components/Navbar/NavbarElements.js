62044932f8c6035c3dc595091a50b53523cc3c74
Yonatan Haddish <yonihailshn@gmail.com>
2021-12-21 13:00:31 -0500

    navbar styling

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -52,6 +52,7 @@ export const MobileIcon= styled.div`
         transform: translate(-100%, 60%);
         font-size: 1.8rem;
         cursor: pointer;
+        color: #fff;
     }
 `;
 
@@ -83,7 +84,33 @@ export const NavLinks= styled(LinkS)`
     &:active {
         border-bottom: 3px solid #01bf71;
     }
-
-
 `;
 
+export const NavBtn= styled.nav`
+    display: flex;
+    align-items: center;
+
+    @media screen and (max-width: 768px) {
+        display: none;
+    }
+`;
+
+export const NavBtnLink= styled(LinkR)`
+    border-radius: 50px;
+    background: #01bf71;
+    white-space: nowrap;
+    padding: 10px 22px;
+    color: #010606;
+    font-size: 16px;
+    align: none;
+    cursor: pointer;
+    border: none;
+    text-decoration: none;
+    transition: all 0.2s ease-in-out;
+
+    &:hover {
+        transition: all 0.2s ease-in-out;
+        background: #fff;
+        color: #010606;
+    }
+`;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,35 +1,48 @@
-import React from 'react'
-import { FaBars } from 'react-icons/fa';
+import React from "react";
+import { FaBars } from "react-icons/fa";
 
-import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from './NavbarElements';
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElements";
 
 const Navbar = () => {
-    return (
-        <>
-            <Nav>
-                <NavbarContainer>
-                    <NavLogo to="/">YH-Bank</NavLogo>
-                    <MobileIcon>
-                        <FaBars />
-                    </MobileIcon>
-                    <NavMenu>
-                        <NavItem>
-                            <NavLinks to="about">About</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to="">Service</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to="">AA</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to="">Signup</NavLinks>
-                        </NavItem>
-                    </NavMenu>
-                </NavbarContainer>
-            </Nav> 
-        </>
-    );
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">YH-Bank</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="discover">Discover</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="services">Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="signup">Sign Up</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="/signin">Sign In</NavBtnLink>
+          </NavBtn>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
 };
 
 export default Navbar;
