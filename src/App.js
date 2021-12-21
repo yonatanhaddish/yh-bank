14fdae5a5e13a5d93a7a137684331974b60aaf9c
Yonatan Haddish <yonihailshn@gmail.com>
2021-12-21 12:41:02 -0500

    working on navbar section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Home from './pages';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
-      <Home />
+      <Navbar />
     </Router>
   );
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import { FaBars } from 'react-icons/fa';
+
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from './NavbarElements';
 
 const Navbar = () => {
     return (
         <>
             <Nav>
                 <NavbarContainer>
-                    <NavLogo>YH-Bank</NavLogo>
+                    <NavLogo to="/">YH-Bank</NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to="about">About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="">Service</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="">AA</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="">Signup</NavLinks>
+                        </NavItem>
+                    </NavMenu>
                 </NavbarContainer>
             </Nav> 
         </>
