492602ebfa12a3a31b37e91227d65c28346ad8a7
Yonatan Haddish <yonihailshn@gmail.com>
2022-01-18 11:10:37 -0500

    some styling on InfoSection component

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,26 +1,47 @@
-import React from 'react'
+import React from "react";
+
+import { Button } from '../ButtonElements';
+import {
+  InfoContainer,
+  InfoWrapper,
+  InfoRow,
+  Column1,
+  Column2,
+  TextWrapper,
+  TopLine,
+  Heading,
+  Subtitle,
+  BtnWrap,
+  ImgWrap,
+  Img
+} from "./InfoElements";
 
 const InfoSection = () => {
-    return (
-        <>
-           <InfoContainer id={id}>
-               <InfoWrapper>
-                   <InfoRow>
-                       <Column1>
-                            <TextWrapper>
-                                <TopLine>Topline</TopLine>
-                                <Heading>Heading</Heading>
-                                <Subtitle>Subtitle</Subtitle>
-                                <BtmWrap>
-                                    <Button to="home">Button</Button>
-                                </BtmWrap>
-                            </TextWrapper>
-                       </Column1>
-                   </InfoRow>
-               </InfoWrapper>
-           </InfoContainer>
-        </>
-    )
-}
+  return (
+    <>
+      <InfoContainer>
+        <InfoWrapper>
+          <InfoRow>
+            <Column1>
+              <TextWrapper>
+                <TopLine>Topline</TopLine>
+                <Heading>Heading</Heading>
+                <Subtitle>Subtitle</Subtitle>
+                <BtnWrap>
+                  <Button to="home">Button</Button>
+                </BtnWrap>
+              </TextWrapper>
+            </Column1>
+            <Column2>
+                <ImgWrap>
+                    <Img />
+                </ImgWrap>
+            </Column2>
+          </InfoRow>
+        </InfoWrapper>
+      </InfoContainer>
+    </>
+  );
+};
 
-export default InfoSection
+export default InfoSection;
