2f3c48aa5bed75ad9ec85c03bee821a09d10a7c3
Yonatan Haddish <yonihailshn@gmail.com>
2022-01-20 12:12:39 -0500

    signin page and SigninPage component created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 // import React from 'react';
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages';
+import SigninPage from './pages/signin';
 
 function App() {
   return (
     <Router>
-      <Home />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="signin" element={<SigninPage />} />
+      </Routes>
     </Router>
   );
 }
--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -1,5 +1,5 @@
 export const homeObjOne = {
-  id: "abouts",
+  id: "about",
   lightBg: false,
   lightText: true,
   lightTextDesc: false,
