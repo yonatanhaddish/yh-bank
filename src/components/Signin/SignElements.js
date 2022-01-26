be0c7a35ae0d00b4a0932778d765c40614b08f7b
Yonatan Haddish <yonihailshn@gmail.com>
2022-01-26 15:29:44 -0500

    some styling into Signin component

--- src/components/Signin/SignElements.js
+++ src/components/Signin/SignElements.js
@@ -12,3 +12,56 @@ export const Container= styled.div`
     overflow: hidden;
     background: linear-gradient(108deg, rgba(1, 147, 86, 1) 0%, rgba(10, 201, 122, 1) 100);
 `;
+
+export const FormWrap= styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    @media screen and (max-width: 400px) {
+        height: 80%;
+    }
+`;
+
+export const Icon= styled(Link)`
+    margin-left: 32px;
+    margin-top: 32px;
+    text-decoration: none;
+    color: #fff;
+    font-weight: 700;
+    font-size: 32px;
+
+    @media screen and (max-width: 480px) {
+        margin-left: 16px;
+        margin-top: 8px;
+    }
+`;
+
+export const FormContent= styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    @media screen and (max-width: 480px) {
+        padding: 10px;
+    }
+`;
+
+export const Form= styled.form`
+    background: #010101;
+    max-width: 400px;
+    height: auto;
+    width: 100%;
+    z-index: 1;
+    display: grid;
+    margin: 0 auto;
+    padding: 80px 32px;
+    border-radius: 4px;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.9);
+
+    @media screen and (max-width: 400px) {
+        padding: 32px 32px;
+    }
+`;
